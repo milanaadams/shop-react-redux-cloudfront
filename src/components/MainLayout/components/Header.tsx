b71f4eaf3f34370b4f52bdc071bb773d5522df0b
Natallia A <milanaadams@gmail.com>
2022-08-22 20:31:08 +0300

    feat: add products api

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            Marketplace!
+            Backpack Store!
           </Link>
         </Typography>
 
