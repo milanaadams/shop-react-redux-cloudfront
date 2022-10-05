ec17ba1f9bd16f963058961aac81c17eb1a07e19
Natallia A <milanaadams@gmail.com>
2022-10-05 20:29:11 +0300

    feat: integrate import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://5ozs9x5ate.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://i44nky5d2k.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
