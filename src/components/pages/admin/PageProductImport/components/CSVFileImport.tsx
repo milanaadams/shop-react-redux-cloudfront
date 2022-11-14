b2eb19e54d85046aad7f4dfc9c29f2f497ac003a
Natallia A <milanaadams@gmail.com>
2022-11-14 20:31:33 +0300

    Add authorization header

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -10,6 +10,7 @@ type CSVFileImportProps = {
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File | undefined>(undefined);
+  const [authError, setAuthError] = React.useState<string>("");
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -26,29 +27,44 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const uploadFile = async () => {
     console.log("uploadFile to", url);
 
+    const authorizationToken = localStorage.getItem("authorization_token");
+
     // Get the presigned URL
-    const response = await axios({
-      method: "GET",
-      url,
-      params: {
-        name: encodeURIComponent(file?.name as string),
-      },
-    });
-    console.log("Resonse: ", response.data);
-    console.log("File to upload: ", file?.name);
-    console.log("Uploading to: ", response.data);
-    const result = await fetch(response.data, {
-      method: "PUT",
-      body: file,
-    });
-    console.log("Result: ", result);
-    setFile(undefined);
+    try {
+      const response = await axios({
+        method: "GET",
+        headers: {
+          Authorization: authorizationToken
+            ? `Basic ${authorizationToken}`
+            : "",
+        },
+        url,
+        params: {
+          name: encodeURIComponent(file?.name as string),
+        },
+      });
+      console.log("Resonse: ", response.data);
+      console.log("File to upload: ", file?.name);
+      console.log("Uploading to: ", response.data);
+      const result = await fetch(response.data, {
+        method: "PUT",
+        body: file,
+      });
+      console.log("Result: ", result);
+      setAuthError("");
+      setFile(undefined);
+    } catch (err: any) {
+      console.log("Authorization error");
+      const message = err.response?.data?.message;
+      setAuthError(`Authorization error: ${message}`);
+    }
   };
   return (
     <Box>
       <Typography variant="h6" gutterBottom>
         {title}
       </Typography>
+      {authError && <div>Authorization error: {}</div>}
       {!file ? (
         <input type="file" onChange={onFileChange} />
       ) : (
